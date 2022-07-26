adabf808416fbf5295939530e5f51f0fdbae1d51
Mr-Spoony <alexander.i.littlewood@gmail.com>
2022-07-27 11:44:41 +1200

    3 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+// import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class CartService {
+
+//   constructor() { }
+// }
+import { Product } from './products';
+
 export class CartService {
+  items: Product[] = [];
 
-  constructor() { }
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+  getItems() {
+    return this.items;
+  }
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
